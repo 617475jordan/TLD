91851cb06072174f875775b11db7d72017a08559
617475jordan <617475jordan@sina.com>
2017-08-15 16:46:37 +0800

    change

---
+++ TLD/OpenTLD/run_tld.h
@@ -0,0 +1,19 @@
+#ifndef _RUN_TLD_H_
+#define _RUN_TLD_H_
+#include <opencv2/opencv.hpp>
+#include "tld_utils.h"
+#include <iostream>
+#include <sstream>
+#include "TLD.h"
+#include <stdio.h>
+using namespace cv;
+using namespace std;
+//Global variables
+Rect box;
+bool drawing_box = false;
+bool gotBB = false;
+bool tl = true;
+bool rep = false;
+bool fromfile = false;
+string video;
+#endif
